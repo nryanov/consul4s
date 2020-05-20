6bba00a6d1e256bd31fd568dd25970bd2305197f
Nick <grifon52@gmail.com>
2020-05-21 02:12:54 +0300

    Update event model

--- modules/circe/src/main/scala/consul4s/circe/model/Event.scala
+++ modules/circe/src/main/scala/consul4s/circe/model/Event.scala
@@ -1,9 +1,29 @@
 package consul4s.circe.model
 
 import consul4s.model.event.UserEvent
+import io.circe.Decoder.Result
 import io.circe._
-import io.circe.generic.semiauto._
 
 trait Event {
-  implicit val userEventDecoder: Decoder[UserEvent] = deriveDecoder[UserEvent]
+  implicit val userEventDecoder: Decoder[UserEvent] = new Decoder[UserEvent] {
+    override def apply(c: HCursor): Result[UserEvent] = for {
+      id <- c.downField("ID").as[String]
+      name <- c.downField("Name").as[String]
+      payload <- c.downField("Payload").as[Option[String]]
+      nodeFilter <- c.downField("NodeFilter").as[String]
+      serviceFilter <- c.downField("ServiceFilter").as[String]
+      tagFilter <- c.downField("TagFilter").as[String]
+      version <- c.downField("Version").as[Long]
+      lTime <- c.downField("LTime").as[Long]
+    } yield UserEvent(
+      id,
+      name,
+      payload,
+      nodeFilter,
+      serviceFilter,
+      tagFilter,
+      version,
+      lTime
+    )
+  }
 }
--- modules/core/src/main/scala/consul4s/model/event/UserEvent.scala
+++ modules/core/src/main/scala/consul4s/model/event/UserEvent.scala
@@ -4,15 +4,15 @@ import java.nio.charset.StandardCharsets
 import java.util.Base64
 
 final case class UserEvent(
-  ID: String,
-  Name: String,
-  Payload: Option[String],
-  NodeFilter: String,
-  ServiceFilter: String,
-  TagFilter: String,
-  Version: Int,
-  LTime: Long
+  id: String,
+  name: String,
+  payload: Option[String],
+  nodeFilter: String,
+  serviceFilter: String,
+  tagFilter: String,
+  version: Long,
+  lTime: Long
 ) {
   lazy val decodedPayload: Option[String] =
-    Payload.map(v => new String(Base64.getDecoder.decode(v.getBytes(StandardCharsets.UTF_8)), StandardCharsets.UTF_8))
+    payload.map(v => new String(Base64.getDecoder.decode(v.getBytes(StandardCharsets.UTF_8)), StandardCharsets.UTF_8))
 }
