ef350ca3bc05cebbb6e0c0184763f5457c7c482d
Nick <grifon52@gmail.com>
2020-05-25 00:25:50 +0300

    SprayJson integration

--- modules/spray-json/src/main/scala/consul4s/sprayJson/model/Common.scala
+++ modules/spray-json/src/main/scala/consul4s/sprayJson/model/Common.scala
@@ -1,6 +1,34 @@
 package consul4s.sprayJson.model
 
-import consul4s.model.{CheckStatus, ServiceKind}
-import spray.json.{JsString, JsValue, RootJsonFormat, deserializationError}
+import consul4s.model.{CheckStatus, ServiceKind, SessionBehavior}
+import spray.json.{DefaultJsonProtocol, JsString, JsValue, RootJsonFormat, deserializationError}
 
-trait Common {}
+trait Common extends DefaultJsonProtocol {
+  implicit object CheckStatusFormat extends RootJsonFormat[CheckStatus] {
+    override def write(obj: CheckStatus): JsValue = JsString(obj.value)
+
+    override def read(json: JsValue): CheckStatus = json match {
+      case JsString(value) => CheckStatus.withValue(value)
+      case _               => deserializationError("CheckStatus expected")
+    }
+  }
+
+  implicit object ServiceKindFormat extends RootJsonFormat[ServiceKind] {
+    override def write(obj: ServiceKind): JsValue = JsString(obj.value)
+
+    override def read(json: JsValue): ServiceKind = json match {
+      case JsString(value) => ServiceKind.withValue(value)
+      case _               => deserializationError("ServiceKind expected")
+    }
+  }
+
+  implicit object SessionBehaviorFormat extends RootJsonFormat[SessionBehavior] {
+    override def write(obj: SessionBehavior): JsValue = JsString(obj.value)
+
+    override def read(json: JsValue): SessionBehavior = json match {
+      case JsString(value) => SessionBehavior.withValue(value)
+      case _               => deserializationError("SessionBehavior expected")
+    }
+  }
+
+}
